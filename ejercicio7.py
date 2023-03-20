7f34a9097875be38a71b9091ad8928efd945eb61
juanjo <juanjo>
2023-03-20 20:29:16 -0300

    Agregado de cuenta joven

--- ejercicio7.py
+++ ejercicio7.py
@@ -11,9 +11,10 @@ negativa, no se hará nada.
  retirar(cantidad): se retira una cantidad a la cuenta. La cuenta puede estar en números rojos'''
 
 class Persona():
-    def __init__(self , nombre , apellido):
+    def __init__(self , nombre , apellido, edad):
         self.__nombre = nombre
         self.__apellido = apellido
+        self.__edad=edad
 
     @property
     def nombre(self):
@@ -22,7 +23,9 @@ class Persona():
     @property
     def apellido(self):
         return self.__apellido
-
+    @property
+    def edad(self):
+        return self.__edad
 
 
 class Cuenta():
@@ -46,27 +49,78 @@ class Cuenta():
     def retirar(self,monto):
         if (monto>0):
             self.__cantidad = self.__cantidad - monto
-            print(self.mostrar())
+            print(f"\n Se retiraron: {monto}\n" ,self.mostrar())
         else:
-            print("Ingrese un monto superior a 0")
+            print("\n Ingrese un monto superior a 0\n")
 
     def ingresar(self,monto):
         if (monto>0):
             self.__cantidad = self.__cantidad + monto
-            print(self.mostrar())
+            print(f"\n Se ingresaron: {monto}\n" ,self.mostrar())
         else:
-            print("Ingrese un monto superior a 0")
+            print("\n Ingrese un monto superior a 0\n")
 
     def mostrar(self):
-        return f"Titular  {self.titular.nombre} {self.titular.apellido} saldo {self.__cantidad} $"
+        return f"Titular:  {self.titular.nombre} {self.titular.apellido} \n Saldo: {self.__cantidad} $"
+
+'''8. Vamos a definir ahora una “Cuenta Joven”, para ello vamos a crear una nueva clase 
+CuantaJoven que deriva de la clase creada en el punto 7. Cuando se crea esta nueva clase, 
+además del titular y la cantidad se debe guardar una bonificación que estará expresada en 
+tanto por ciento. Crear los siguientes métodos para la clase:
+ Un constructor.
+ Los setters y getters para el nuevo atributo.
+ En esta ocasión los titulares de este tipo de cuenta tienen que ser mayor de edad, por lo 
+tanto hay que crear un método es_titular_valido() que devuelve verdadero si el titular es 
+mayor de edad pero menor de 25 años y falso en caso contrario.
+ Además, la retirada de dinero sólo se podrá hacer si el titular es válido.
+ El método mostrar() debe devolver el mensaje de “Cuenta Joven” y la bonificación de la 
+cuenta'''
+class CuentaJoven(Cuenta):
+    def __init__(self, titular, bonificacion):
+        super().__init__(titular)
+        self.__bonificacion=bonificacion
+    
+    @property
+    def bonificacion (self):
+        return self.__bonificacion
+
+    @bonificacion.setter
+    def bonificacion (self,bono):
+        self.__bonificacion=bono
+
+    def es_titular_valido(self):
+        if (self.__titular.edad > 18 and self.__titular.edad < 25 ):
+            return True
+        else:
+            return False
+
+    def retirar(self, monto):
+        if(self.es_titular_valido==True):
+            return super().retirar(monto)
+        else:
+            return print ("\nTitular no valido no puede extraer dinero")
+        
+
+    def mostrar(self):
+         return f"Cuenta Joven:  {self.titular.nombre} {self.titular.apellido} \n Saldo: {self.cantidad} $ \n Bonificacion: {self.bonificacion}%"
+    
+    
 
 
-titular1 = Persona("Juan", "Perez")
+titular1 = Persona("Juan", "Perez",25)
 cuenta1 = Cuenta(titular1)  
 
 cuenta1.retirar(100.30)
 cuenta1.ingresar(500)
 cuenta1.retirar(-1)
+
+
+titular2 = Persona("jose", "Marrone",45)
+cuenta2 = CuentaJoven(titular2, 0.2)  
+cuenta2.ingresar(500)
+cuenta2.retirar(100.30)
+cuenta2.ingresar(500)
+
 
 
 
